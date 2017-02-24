5123ff63fb769ba5a49e729d5f7451af71a5cb5d
FirstActivity <zhangyuanqiao0912@163.com>
2017-02-24 13:56:17 +0800

    APP Test

--- app/src/main/java/com/example/administrator/activitytest/SecondActivity.java
+++ app/src/main/java/com/example/administrator/activitytest/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.administrator.activitytest;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,5 +31,51 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();//启动SecondActivity的Intent，然后调用getStringExtra方法传入值，或者是getBooleanExtra()
         String Data = intent.getExtras().getString("extra_data");
         Log.d("SecondActivity",  Data);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FirstActivity");
+        setResult(RESULT_OK,intent);//第一个参数是RESULT_OK，第二个参数是数据回传
+        finish();//销毁当前活动
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();//
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();//准备好和用户交互时调用，这个活动一定出于栈顶且在活动状态；
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();//准备去启动或者恢复另一个活动的时候去调用；
+        // 消耗一些CPU，来保存一些关键的数据，同时在执行的时候一定要快；
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();//活动完全不可见的时候调用，和onPause()的区别在于：
+        // 如果启动一个新的活动是对话框的形式的话，调用onPause()而不调用onStop()
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();//在活动销毁的时候调用，之后的活动状态将变为销毁状态
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();//活动由停止状态到活动状态转换时调用，也就是活动重新被启动。
     }
 }
